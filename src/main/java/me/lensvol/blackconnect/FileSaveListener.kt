28b3b2aafbb525380a2f8ff367ff87ab3a4644fc
Kirill Borisov <lensvol@gmail.com>
2020-05-22 12:43:58 +0200

    Implement reformatting on each file save.

---
+++ src/main/java/me/lensvol/blackconnect/FileSaveListener.kt
@@ -0,0 +1,14 @@
+package me.lensvol.blackconnect
+
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.fileEditor.FileDocumentManagerListener
+import com.intellij.openapi.project.Project
+
+class FileSaveListener(project: Project) : FileDocumentManagerListener {
+    private val currentProject: Project = project
+
+    override fun beforeDocumentSaving(document: Document) {
+        val configuration = BlackConnectSettingsConfiguration.getInstance(currentProject)
+        BlackdReformatter(currentProject, configuration).process(document)
+    }
+}
