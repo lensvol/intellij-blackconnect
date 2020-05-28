1721ca730b76d024b21d3211a9814ad975cb639f
Kirill Borisov <lensvol@gmail.com>
2020-05-28 20:34:30 +0200

    Fix reformatting being done on non-Python files.

--- src/main/java/me/lensvol/blackconnect/FileSaveListener.kt
+++ src/main/java/me/lensvol/blackconnect/FileSaveListener.kt
@@ -1,6 +1,7 @@
 package me.lensvol.blackconnect
 
 import com.intellij.openapi.editor.Document
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileDocumentManagerListener
 import com.intellij.openapi.project.Project
 
@@ -9,8 +10,15 @@ class FileSaveListener(project: Project) : FileDocumentManagerListener {
 
     override fun beforeDocumentSaving(document: Document) {
         val configuration = BlackConnectSettingsConfiguration.getInstance(currentProject)
-        if(configuration.triggerOnEachSave){
-            BlackdReformatter(currentProject, configuration).process(document)
+        if (!configuration.triggerOnEachSave) {
+            return
+        }
+
+        val vFile = FileDocumentManager.getInstance().getFile(document)
+        vFile?.name?.let {filename ->
+            if (filename.endsWith(".py") || filename.endsWith(".pyi")) {
+                BlackdReformatter(currentProject, configuration).process(document)
+            }
         }
     }
 }
