7428310b24182db0aabba87fcfabab5e2e8e1612
Kirill Borisov <lensvol@gmail.com>
2020-06-20 18:53:58 +0200

    Introduce rudimentary logging.

--- src/main/java/me/lensvol/blackconnect/PluginStartupActivity.kt
+++ src/main/java/me/lensvol/blackconnect/PluginStartupActivity.kt
@@ -9,20 +9,36 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import me.lensvol.blackconnect.settings.BlackConnectGlobalSettings
 import me.lensvol.blackconnect.ui.NewSettingsReminderNotification
+import com.intellij.openapi.diagnostic.Logger
+import java.util.Properties
 
 class PluginStartupActivity : StartupActivity, DumbAware {
+    private val logger = Logger.getInstance(PluginStartupActivity::class.java.name)
+
     override fun runActivity(project: Project) {
         val applicationInfo = ApplicationInfo.getInstance()
+
+        val properties = Properties()
+        val versionResource = PluginStartupActivity::class.java.getResourceAsStream("/version.properties")
+        if (versionResource != null) {
+            properties.load(versionResource)
+            versionResource.close()
+            logger.info("BlackConnect plugin (version: " + properties["version"] +") is ready to start.")
+        } else {
+            logger.info("Unknown version of BlackConnect plugin is ready to start.")
+        }
 
         // FIXME: For some reason, project listeners declared in plugin.xml
         // are not being registered, so we do it manually.
         if (applicationInfo.build.baselineVersion < 201) {
+            logger.debug("IDE version is too old, we manually subscribe to FILE_DOCUMENT_SYNC events.")
             project.messageBus.connect().subscribe(AppTopics.FILE_DOCUMENT_SYNC, FileSaveListener(project))
         }
 
         val notificationGroup = NotificationGroup("BlackConnect", NotificationDisplayType.STICKY_BALLOON, false)
         val appLevelConfiguration = BlackConnectGlobalSettings.getInstance()
         if (appLevelConfiguration.showSaveTriggerOptIn) {
+            logger.debug("Showing opt-in balloon.")
             val optInNotification = NewSettingsReminderNotification(
                 notificationGroup,
                 project, appLevelConfiguration
