80771c7f234395ae92d0b56825c2a3ba13cb2ffa
Kirill Borisov <lensvol@gmail.com>
2020-06-22 21:08:06 +0200

    Fix same file being reformatted multiple times if several projects are opened.

--- src/main/java/me/lensvol/blackconnect/FileSaveListener.kt
+++ src/main/java/me/lensvol/blackconnect/FileSaveListener.kt
@@ -3,7 +3,9 @@ package me.lensvol.blackconnect
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileDocumentManagerListener
+import com.intellij.openapi.module.ModuleUtil
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
 import me.lensvol.blackconnect.settings.BlackConnectProjectSettings
 
 class FileSaveListener(project: Project) : FileDocumentManagerListener {
@@ -17,6 +19,15 @@ class FileSaveListener(project: Project) : FileDocumentManagerListener {
 
         val vFile = FileDocumentManager.getInstance().getFile(document)
         vFile?.let {file ->
+            /*
+            When document is going to be saved, it will be passed to each instance
+            of the listener and they may each reformat it according to their projects.
+
+            Following line will reformat code only if it belongs to one of the modules
+            in project associated with this instance.
+             */
+            ModuleUtil.findModuleForFile(vFile, currentProject) ?: return
+
             val reformatter = BlackdReformatter(currentProject, configuration)
             if (reformatter.isFileSupported(file)) {
                 reformatter.process(document)
