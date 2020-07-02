ac5e330fe86b12778b443db41467c9d3095288c2
Kirill Borisov <lensvol@gmail.com>
2020-07-02 22:18:48 +0200

    Refactor error reporter to be more concise.

--- src/main/kotlin/me/lensvol/blackconnect/sentry/SentryErrorReporter.kt
+++ src/main/kotlin/me/lensvol/blackconnect/sentry/SentryErrorReporter.kt
@@ -4,7 +4,6 @@ import com.intellij.diagnostic.IdeaReportingEvent
 import com.intellij.ide.DataManager
 import com.intellij.ide.plugins.IdeaPluginDescriptor
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.ErrorReportSubmitter
 import com.intellij.openapi.diagnostic.IdeaLoggingEvent
 import com.intellij.openapi.diagnostic.SubmittedReportInfo
@@ -12,9 +11,7 @@ import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.Messages
 import com.intellij.util.Consumer
-import io.sentry.connection.EventSendCallback
 import io.sentry.event.Event
 import io.sentry.event.EventBuilder
 import io.sentry.event.interfaces.ExceptionInterface
@@ -40,17 +37,10 @@ class SentryErrorReporter : ErrorReportSubmitter() {
         val context = DataManager.getInstance().getDataContext(parentComponent)
         val project: Project? = CommonDataKeys.PROJECT.getData(context)
 
+        val sentryClient = SentryIntegration.client()
+
         ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Report error to sentry.io") {
             override fun run(indicator: ProgressIndicator) {
-                val event = EventBuilder()
-                event.withLevel(Event.Level.ERROR)
-
-                if (pluginDescriptor is IdeaPluginDescriptor) {
-                    event.withRelease((pluginDescriptor as IdeaPluginDescriptor).version)
-                }
-                // Empty server name to avoid tracking personal data
-                event.withServerName("<redacted>")
-
                 val errors = LinkedList<SentryException>()
                 for (ideaEvent in events) {
                     if (ideaEvent is IdeaReportingEvent) {
@@ -59,31 +49,17 @@ class SentryErrorReporter : ErrorReportSubmitter() {
                     }
                 }
 
-                event.withSentryInterface(ExceptionInterface(errors))
-                val sentryClient = SentryIntegration.client()
-                sentryClient.addEventSendCallback(object: EventSendCallback {
-                    override fun onSuccess(event: Event?) {
-                        ApplicationManager.getApplication().invokeLater {
-                            Messages.showInfoMessage(
-                                parentComponent,
-                                "Thank you for submitting your report!",
-                                "Reporting to Sentry"
-                            )
-                            consumer.consume(SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.NEW_ISSUE))
-                        }
+                val event = EventBuilder().apply {
+                    // Empty server name to avoid tracking personal data
+                    withServerName("<redacted>")
+                    withLevel(Event.Level.ERROR)
+                    withMessage(additionalInfo)
+                    withSentryInterface(ExceptionInterface(errors))
+                    if (pluginDescriptor is IdeaPluginDescriptor) {
+                        withRelease((pluginDescriptor as IdeaPluginDescriptor).version)
                     }
-
-                    override fun onFailure(event: Event?, exception: Exception?) {
-                        ApplicationManager.getApplication().invokeLater {
-                            Messages.showErrorDialog(
-                                parentComponent,
-                                exception.toString(),
-                                "Reporting to Sentry"
-                            )
-                            consumer.consume(SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.FAILED))
-                        }
-                    }
-                })
+                }
+                sentryClient.showResultInUi(parentComponent, consumer)
                 sentryClient.sendEvent(event)
             }
         })
--- src/main/kotlin/me/lensvol/blackconnect/sentry/SentryIntegration.kt
+++ src/main/kotlin/me/lensvol/blackconnect/sentry/SentryIntegration.kt
@@ -1,23 +1,52 @@
 package me.lensvol.blackconnect.sentry
 
 import com.intellij.openapi.application.ApplicationInfo
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.SubmittedReportInfo
+import com.intellij.openapi.ui.Messages
+import com.intellij.util.Consumer
 import io.sentry.SentryClient
 import io.sentry.SentryClientFactory
+import io.sentry.connection.EventSendCallback
+import io.sentry.event.Event
+import java.awt.Component
 
 const val DSN = "https://74ace242ccf94335bf6917d5b7f4aad6@o413179.ingest.sentry.io/5305122"
 
+fun SentryClient.showResultInUi(parentComponent: Component, consumer: Consumer<SubmittedReportInfo>) {
+    this.addEventSendCallback(object: EventSendCallback {
+        override fun onSuccess(event: Event?) {
+            ApplicationManager.getApplication().invokeLater {
+                Messages.showInfoMessage(
+                    parentComponent,
+                    "Thank you for submitting your report!",
+                    "Reporting to Sentry"
+                )
+                consumer.consume(SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.NEW_ISSUE))
+            }
+        }
+
+        override fun onFailure(event: Event?, exception: Exception?) {
+            ApplicationManager.getApplication().invokeLater {
+                Messages.showErrorDialog(
+                    parentComponent,
+                    exception.toString(),
+                    "Reporting to Sentry"
+                )
+                consumer.consume(SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.FAILED))
+            }
+        }
+    })
+}
+
 class SentryIntegration {
     companion object {
-        private val sentryClient: SentryClient = SentryClientFactory.sentryClient(DSN)
-
-        init {
+        fun client(): SentryClient {
+            val sentryClient: SentryClient = SentryClientFactory.sentryClient(DSN)
             sentryClient.addBuilderHelper { e ->
                 val build = ApplicationInfo.getInstance().build.asString()
                 e.withTag("ide.build", build)
             }
-        }
-
-        fun client(): SentryClient {
             return sentryClient
         }
     }
