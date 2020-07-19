245b64772ee92d965f35c37ecf8e2858ea966c9c
Kirill Borisov <lensvol@gmail.com>
2020-07-19 13:35:45 +0200

    Split formatting whole file and fragment into separate actions.

--- src/main/kotlin/me/lensvol/blackconnect/actions/ReformatSelectedFragmentAction.kt
+++ src/main/kotlin/me/lensvol/blackconnect/actions/ReformatSelectedFragmentAction.kt
@@ -14,8 +14,8 @@ import me.lensvol.blackconnect.CodeReformatter
 import me.lensvol.blackconnect.DocumentUtil
 import me.lensvol.blackconnect.settings.BlackConnectProjectSettings
 
-class FormatUsingBlackdAction : AnAction(), DumbAware {
-    private val logger = Logger.getInstance(FormatUsingBlackdAction::class.java.name)
+class ReformatSelectedFragmentAction : AnAction(), DumbAware {
+    private val logger = Logger.getInstance(ReformatSelectedFragmentAction::class.java.name)
 
     override fun actionPerformed(event: AnActionEvent) {
         val project = event.project ?: return
@@ -26,14 +26,18 @@ class FormatUsingBlackdAction : AnAction(), DumbAware {
         val vFile: VirtualFile? = FileDocumentManager.getInstance().getFile(editor.document)
         val fileName = vFile?.name ?: "unknown"
 
-        codeReformatter.process(
-            fileName,
-            editor.document.text,
-            fileName.endsWith(".pyi")
-        ) { reformatted ->
+        val primaryCaret = editor.caretModel.primaryCaret
+        val selectionStart = primaryCaret.selectionStart
+        val selectionEnd = if (primaryCaret.selectionEndPosition.column == 0) {
+            primaryCaret.selectionEnd - 1
+        } else {
+            primaryCaret.selectionEnd
+        }
+
+        val fragment = editor.document.getText(TextRange.create(selectionStart, selectionEnd))
+        codeReformatter.processFragment(fileName, fragment, fileName.endsWith(".pyi")) { reformatted ->
             DocumentUtil.updateCodeInDocument(project, editor.document, reformatted) {
-                logger.debug("Code is going to be updated in ${editor.document}")
-                editor.document.setText(reformatted)
+                editor.document.replaceString(selectionStart, selectionEnd, reformatted)
             }
         }
     }
@@ -42,12 +46,8 @@ class FormatUsingBlackdAction : AnAction(), DumbAware {
         event.presentation.isEnabled = false
 
         val project: Project = event.project ?: return
-        val vFile: VirtualFile = event.getData(PlatformDataKeys.VIRTUAL_FILE) ?: return
-        val configuration = BlackConnectProjectSettings.getInstance(project)
+        val editor = FileEditorManagerEx.getInstance(project).selectedTextEditor ?: return
 
-        event.presentation.isEnabled = CodeReformatter(
-            project,
-            configuration
-        ).isFileSupported(vFile)
+        event.presentation.isEnabled = editor.selectionModel.hasSelection()
     }
 }
