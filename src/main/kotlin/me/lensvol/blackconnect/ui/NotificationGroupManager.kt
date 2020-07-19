d0fdb9a925d33dbc1eace932ba7d2a290a856c02
Kirill Borisov <lensvol@gmail.com>
2020-07-19 22:17:18 +0200

    Remove unused notification group.

--- src/main/kotlin/me/lensvol/blackconnect/ui/NotificationGroupManager.kt
+++ src/main/kotlin/me/lensvol/blackconnect/ui/NotificationGroupManager.kt
@@ -6,16 +6,6 @@ import com.intellij.notification.NotificationGroup
 class NotificationGroupManager {
     companion object {
         const val MAIN_DISPLAY_ID = "BlackConnect"
-        const val NEWS_DISPLAY_ID = "BlackConnect News"
-
-        fun newsGroup(): NotificationGroup {
-            val existingGroup = NotificationGroup.findRegisteredGroup(NEWS_DISPLAY_ID)
-            if (existingGroup != null) {
-                return existingGroup
-            }
-
-            return NotificationGroup(NEWS_DISPLAY_ID, NotificationDisplayType.STICKY_BALLOON, false)
-        }
 
         fun mainGroup(): NotificationGroup {
             val existingGroup = NotificationGroup.findRegisteredGroup(MAIN_DISPLAY_ID)
