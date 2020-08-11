1be65196849b526004ac7d72ac21f54026ea8c19
Kirill Borisov <lensvol@gmail.com>
2020-08-11 19:15:32 +0200

    Rearrange code in FileSaveListener to be more concise.

--- src/main/kotlin/me/lensvol/blackconnect/FileSaveListener.kt
+++ src/main/kotlin/me/lensvol/blackconnect/FileSaveListener.kt
@@ -19,19 +19,17 @@ class FileSaveListener(project: Project) : FileDocumentManagerListener {
             return
         }
 
-        val vFile = FileDocumentManager.getInstance().getFile(document)
-        vFile?.let { file ->
-            /*
-            When document is going to be saved, it will be passed to each instance
-            of the listener and they may each reformat it according to their projects.
+        val vFile = FileDocumentManager.getInstance().getFile(document) ?: return
+        /*
+        When document is going to be saved, it will be passed to each instance
+        of the listener and they may each reformat it according to their projects.
 
-            Following line will ensure that we reformat code only if it belongs to
-            one of the modules in project associated with this instance.
-             */
-            ModuleUtil.findModuleForFile(vFile, currentProject) ?: return
-            if (codeReformatter.isFileSupported(vFile)) {
-                ReformatWholeFileAction.reformatWholeDocument(file.name, currentProject, document)
-            }
+        Following line will ensure that we reformat code only if it belongs to
+        one of the modules in project associated with this instance.
+         */
+        ModuleUtil.findModuleForFile(vFile, currentProject) ?: return
+        if (codeReformatter.isFileSupported(vFile)) {
+            ReformatWholeFileAction.reformatWholeDocument(vFile.name, currentProject, document)
         }
     }
 }
