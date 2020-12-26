36ad30918dd4bc6dfc8d967abbfabb8b2e6626f7
Kirill Borisov <lensvol@gmail.com>
2020-12-26 18:50:37 +0100

    Remove startup workaround for the earlier versions of IDEA.

--- src/main/kotlin/me/lensvol/blackconnect/PluginStartupActivity.kt
+++ src/main/kotlin/me/lensvol/blackconnect/PluginStartupActivity.kt
@@ -1,7 +1,5 @@
 package me.lensvol.blackconnect
 
-import com.intellij.AppTopics
-import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
@@ -12,8 +10,6 @@ class PluginStartupActivity : StartupActivity, DumbAware {
     private val logger = Logger.getInstance(PluginStartupActivity::class.java.name)
 
     override fun runActivity(project: Project) {
-        val applicationInfo = ApplicationInfo.getInstance()
-
         val properties = Properties()
         val versionResource = PluginStartupActivity::class.java.getResourceAsStream("/version.properties")
         if (versionResource != null) {
@@ -22,13 +18,6 @@ class PluginStartupActivity : StartupActivity, DumbAware {
             logger.info("BlackConnect plugin (version: " + properties["version"] + ") is ready to start.")
         } else {
             logger.info("Unknown version of BlackConnect plugin is ready to start.")
-        }
-
-        // FIXME: For some reason, project listeners declared in plugin.xml
-        // are not being registered, so we do it manually.
-        if (applicationInfo.build.baselineVersion < 201) {
-            logger.debug("IDE version is too old, we manually subscribe to FILE_DOCUMENT_SYNC events.")
-            project.messageBus.connect().subscribe(AppTopics.FILE_DOCUMENT_SYNC, FileSaveListener(project))
         }
     }
 }
