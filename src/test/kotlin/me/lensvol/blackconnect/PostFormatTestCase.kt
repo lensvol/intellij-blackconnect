cb61a6b320dda52df304832fc35389f4c7741c83
Remco Wendt <r.wendt@ultimaker.com>
2022-08-13 09:48:16 +0200

    Add test to show blackd doesn't see the indentation when being served a fragment.

--- src/test/kotlin/me/lensvol/blackconnect/PostFormatTestCase.kt
+++ src/test/kotlin/me/lensvol/blackconnect/PostFormatTestCase.kt
@@ -96,7 +96,7 @@ class PostFormatTestCase {
     }
 
     @Test
-    fun `test reformat fragment with Black end of file, respects indents`() = runInEdtAndWait {
+    fun `test reformat fragment with Black respects indents`() = runInEdtAndWait {
         fixture.blackSettings.triggerOnReformat = true
         fixture.configureByText(
             "a.py",
@@ -116,6 +116,36 @@ class PostFormatTestCase {
                         1,
                         2,
                         3,
+                    )
+ 
+            """.trimIndent()
+        )
+    }
+
+
+    @Test
+    fun `test reformat long line with Black respects line length`() = runInEdtAndWait {
+        fixture.blackSettings.triggerOnReformat = true
+        fixture.blackSettings.lineLength = 80
+        // The selected print line in the editor is just over 80 char if you also count the
+        // indents. We expect black to pick up the indentation so it can correctly see the
+        // line length.
+        fixture.configureByText(
+            "a.py",
+            """
+            class Foo:
+                def bar(self):
+                    <selection>print("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod")</selection>
+ 
+            """.trimIndent()
+        )
+        fixture.performEditorAction(IdeActions.ACTION_EDITOR_REFORMAT)
+        fixture.checkResult(
+            """
+            class Foo:
+                def bar(self):
+                    print(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
                     )
  
             """.trimIndent()
