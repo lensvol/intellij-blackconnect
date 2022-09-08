5c92f7ccb34b9e8e7e6a14f074f33467650f62bc
KotlinIsland <kotlinisland@users.noreply.github.com>
2022-09-08 20:54:09 +1000

    Add tests for `preview` support

--- src/test/kotlin/me/lensvol/blackconnect/BlackdClientTestCase.kt
+++ src/test/kotlin/me/lensvol/blackconnect/BlackdClientTestCase.kt
@@ -32,6 +32,7 @@ class BlackdClientTestCase : BlackConnectTestCase() {
             ).respond(
                 HttpResponse.response()
                     .withStatusCode(200)
+                    .withHeader("X-Black-Version", "22.8.0")
                     .withBody("with function(x) as f:\r\n    pass")
             )
         }
@@ -56,6 +57,7 @@ class BlackdClientTestCase : BlackConnectTestCase() {
             ).respond(
                 HttpResponse.response()
                     .withStatusCode(200)
+                    .withHeader("X-Black-Version", "22.8.0")
                     .withBody("with function(x) as f:\n    pass")
             )
         }
@@ -84,5 +86,37 @@ class BlackdClientTestCase : BlackConnectTestCase() {
         val blackdClient = BlackdClient("localhost", 45484, useSsl = false)
 
         Assert.assertEquals(blackdClient.blackdUrl, URL("http://localhost:45484"))
+    }
+
+    private fun mockVersion(version: String) {
+        mockServer.reset()
+        mockServer.apply {
+            this.`when`(
+                HttpRequest.request()
+                    .withMethod("POST")
+                    .withPath("/")
+            ).respond(
+                HttpResponse.response()
+                    .withStatusCode(204)
+                    .withHeader("X-Black-Version", version)
+            )
+        }
+    }
+    @Test
+    fun `test blackd client checks version with preview`() {
+        val blackdClient = BlackdClient("localhost", 45484)
+        val format = { blackdClient.reformat("", previewMode = true) }
+
+        val successes = listOf("22.8.0", "23.7.0", "23.8.0")
+        val failures = listOf("21.7.0", "21.8.0", "22.7.0")
+
+        for (version in successes) {
+            mockVersion(version)
+            Assert.assertTrue(format() is Success)
+        }
+        for (version in failures) {
+            mockVersion(version)
+            Assert.assertTrue(format() is Failure)
+        }
     }
 }
