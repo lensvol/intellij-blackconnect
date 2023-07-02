72cfa68e7d50c730d13e14fac96346eff5b5ef70
Kirill Borisov <lensvol@gmail.com>
2023-07-02 10:56:18 +0200

    Replace tab closure tracking code with a proper action listener.
    Closes #83

---
+++ src/main/kotlin/me/lensvol/blackconnect/BeforeTabCloseActionListener.kt
@@ -0,0 +1,28 @@
+package me.lensvol.blackconnect
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.actionSystem.ex.AnActionListener
+import com.intellij.openapi.components.service
+import com.intellij.openapi.fileEditor.impl.tabActions.CloseTab
+import com.intellij.openapi.vfs.VirtualFile
+
+class BeforeTabCloseActionListener : AnActionListener {
+    override fun beforeActionPerformed(action: AnAction, event: AnActionEvent) {
+        super.beforeActionPerformed(action, event)
+
+        val project = event.project ?: return
+        if (project.isDisposed) {
+            return
+        }
+
+        if (action is CloseTab) {
+            val vFile: VirtualFile? = event.getData(PlatformDataKeys.VIRTUAL_FILE)
+            vFile?.let { file ->
+                val projectService = event.project!!.service<BlackConnectProgressTracker>()
+                projectService.cancelOperationOnTag(file.path)
+            }
+        }
+    }
+}
